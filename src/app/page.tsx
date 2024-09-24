bd26d129c5f47a579b2ba6d878ab3f06f038ffb4
Israel Antunes Neto <israneto20@gmail.com>
2024-09-24 00:57:27 -0300

    Versão V 0.0.1

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import DefaultLayout from "@/components/layouts/DefaultLayout";
+
+export default function Home() {
+  return (
+    <>
+      <DefaultLayout>
+        <p>Hollo Moto</p>
+      </DefaultLayout>
+    </>
+  );
+}
