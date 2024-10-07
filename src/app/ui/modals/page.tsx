33f7aff098b02523014a3398a75ccc7e8161e422
Israel Antunes Neto <62219211+isranetoo@users.noreply.github.com>
2024-10-07 10:58:08 -0700

    test modal

--- src/app/ui/modals/page.tsx
+++ src/app/ui/modals/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+// Define the ModalProps type
+declare type ModalProps = {
+  id: string;
+  title: string;
+  content: React.ReactNode;
+  onCloseText: string;
+};
+
+// Modal component
 const Modal: React.FC<ModalProps> = ({ id, title, content, onCloseText }) => {
   return (
     <dialog id={id} className="modal">
@@ -7,7 +16,9 @@ const Modal: React.FC<ModalProps> = ({ id, title, content, onCloseText }) => {
         <h3 className="text-lg font-bold">{title}</h3>
         <div className="py-4">{content}</div>
         <div className="modal-action">
-          <form method="dialog"></form>
+          <form method="dialog">
+            <button type="submit">{onCloseText}</button>
+          </form>
         </div>
       </div>
     </dialog>
