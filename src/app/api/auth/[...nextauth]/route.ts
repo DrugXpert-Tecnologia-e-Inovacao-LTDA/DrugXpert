8056c80d53da44a6480a0840ec2e1f132516e8fc
Israel Antunes Neto <62219211+isranetoo@users.noreply.github.com>
2024-10-07 10:17:37 -0700

    route auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,48 +1,50 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { loginUser } from "@/lib/actions/user.actions";
 
-export const handler = NextAuth({
+const authOptions: AuthOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
       credentials: {
         email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" },
-      } as any,
+      },
       async authorize(credentials) {
         if (credentials?.email && credentials?.password) {
           const user = await loginUser(credentials.email, credentials.password);
           if (user) {
             return user;
-          } else {
-            return null;
           }
         }
-
-        return null;
+        return null; // Return null if user is not found
       },
     }),
   ],
   session: {
-    strategy: "jwt",
-    maxAge: 24 * 60 * 60,
+    strategy: "jwt", // Use string directly
+    maxAge: 24 * 60 * 60, // 24 hours
   },
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
-        token.id = user.id;
+        token.id = user.id; // Attach user ID to the token
       }
       return token;
     },
     async session({ session, token }: any) {
       if (token) {
-        session.id = token.id;
+        session.id = token.id; // Attach token ID to the session
       }
       return session;
     },
   },
-  secret: process.env.NEXTAUTH_SECRET,
-});
+  secret: process.env.NEXTAUTH_SECRET, // Ensure this is set in your environment variables
+};
 
-export { handler as GET, handler as POST };
+// Define handlers for each HTTP method
+const handler = NextAuth(authOptions);
+
+// Export named handlers for GET and POST
+export const GET = handler;
+export const POST = handler;
