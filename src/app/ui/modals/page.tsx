27b18ca2073dd559e1a562746958f5b28923e30c
Israel Antunes Neto <62219211+isranetoo@users.noreply.github.com>
2024-10-07 10:52:39 -0700

    v2 modal fix

--- src/app/ui/modals/page.tsx
+++ src/app/ui/modals/page.tsx
@@ -1,35 +1,17 @@
 import React from "react";
 
-interface ModalProps {
-  id: string;
-  title: string;
-  content: React.ReactNode;
-  onCloseText?: string; // Optional prop
-}
-
-const modal: React.FC<ModalProps> = ({ id, title, content, onCloseText }) => {
-  const closeModal = () => {
-    const dialog = document.getElementById(id) as HTMLDialogElement;
-    if (dialog) {
-      dialog.close();
-    }
-  };
-
+const Modal: React.FC<ModalProps> = ({ id, title, content, onCloseText }) => {
   return (
     <dialog id={id} className="modal">
       <div className="modal-box bg-[#ffffff] text-black dark:bg-[#181818] dark:text-white">
         <h3 className="text-lg font-bold">{title}</h3>
         <div className="py-4">{content}</div>
         <div className="modal-action">
-          <form method="dialog">
-            <button type="button" onClick={closeModal}>
-              {onCloseText || "Close"}
-            </button>
-          </form>
+          <form method="dialog"></form>
         </div>
       </div>
     </dialog>
   );
 };
 
-export default modal; // Ensure there's only one default export here
+export default Modal;
