a28938eeb0534c14b4c5c04869d91c4329d40ad4
Israel Antunes Neto <israneto20@gmail.com>
2024-10-25 00:09:28 -0300

    metadata added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
-import Index from "@/components/Dashboard";
-import { Metadata } from "next";
-import DefaultLayout from "@/components/layouts/DefaultLayout";
+"use client"; 
 
-export const metadata: Metadata = {
-  title:
-    "DrugXpert | Home",
-  description: "This is Next.js Home for TailAdmin Dashboard Template",
-};
+import Index from "@/components/Dashboard";
+import DefaultLayout from "@/components/layouts/DefaultLayout";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export default function Home() {
   return (
-    <>
+    <HelmetProvider>
       <DefaultLayout>
+        <Helmet>
+          <title>DrugXpert | Home</title>
+          <meta
+            name="description"
+            content="This is Next.js Home for TailAdmin Dashboard Template"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Helmet>
         <Index />
       </DefaultLayout>
-    </>
+    </HelmetProvider>
   );
 }
