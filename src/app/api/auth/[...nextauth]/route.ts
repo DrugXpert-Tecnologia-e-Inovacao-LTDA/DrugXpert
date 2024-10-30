df402f5d83783913eb546081644f0f83d6535b1b
Israel Antunes Neto <62219211+isranetoo@users.noreply.github.com>
2024-10-29 23:16:29 -0300

    Google OAuth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,14 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google"; // Import Google provider
 import { loginUser } from "@/lib/actions/user.actions";
 
 const authOptions: AuthOptions = {
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
@@ -17,34 +22,28 @@ const authOptions: AuthOptions = {
             return user;
           }
         }
-        return null; // Return null if user is not found
+        return null;
       },
     }),
   ],
   session: {
-    strategy: "jwt", // Use string directly
-    maxAge: 24 * 60 * 60, // 24 hours
+    strategy: "jwt",
+    maxAge: 24 * 60 * 60,
   },
   callbacks: {
     async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id; // Attach user ID to the token
-      }
+      if (user) token.id = user.id;
       return token;
     },
     async session({ session, token }: any) {
-      if (token) {
-        session.id = token.id; // Attach token ID to the session
-      }
+      if (token) session.id = token.id;
       return session;
     },
   },
-  secret: process.env.NEXTAUTH_SECRET, // Ensure this is set in your environment variables
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
-// Define handlers for each HTTP method
 const handler = NextAuth(authOptions);
 
-// Export named handlers for GET and POST
 export const GET = handler;
 export const POST = handler;
